87cf5da41441faeaba548103238933ec420d12cf
VASKO\eqinox <eqinox@abv.bg>
2025-01-11 19:03:51 +0200

    logout functionality and allow implicit any types

--- src/components/shared/header/menu.tsx
+++ src/components/shared/header/menu.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import { EllipsisVertical, ShoppingCart } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import ModeToggle from "./mode-toggle";
@@ -10,6 +10,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import UserButton from "./user-button";
 
 const Menu = () => {
   return (
@@ -21,11 +22,7 @@ const Menu = () => {
             <ShoppingCart /> Cart
           </Link>
         </Button>
-        <Button asChild>
-          <Link href="/sign-in">
-            <UserIcon /> Sign In
-          </Link>
-        </Button>
+        <UserButton />
       </nav>
       <div className="md:hidden">
         <Sheet>
@@ -41,11 +38,7 @@ const Menu = () => {
               </Link>
             </Button>
 
-            <Button asChild>
-              <Link href="/sign-in">
-                <UserIcon /> Sign In
-              </Link>
-            </Button>
+            <UserButton />
             <SheetDescription></SheetDescription>
           </SheetContent>
         </Sheet>
