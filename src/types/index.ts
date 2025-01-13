5c26826c0488547e5d36d78f7c84ef669041f930
VASKO\eqinox <eqinox@abv.bg>
2025-01-13 20:29:36 +0200

    adding cart as schema and model for prisma

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,16 @@
 import { z } from "zod";
 
-import { insertProductsSchema } from "@/lib/validators";
+import {
+  cartItemSchema,
+  insertCartSchema,
+  insertProductsSchema,
+} from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductsSchema> & {
   id: string;
   rating: string;
   createdAt: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
