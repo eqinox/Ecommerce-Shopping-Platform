808509273a9f8353b80fdb73e19e3b19c6ddfaf6
VASKO\eqinox <eqinox@abv.bg>
2025-01-17 20:44:19 +0200

    add order history

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -31,3 +31,5 @@ export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
   : ["Paypal", "Stripe", "CashOnDelivery"];
 export const DEFAULT_PAYMENT_METHOD =
   process.env.DEFAULT_PAYMENT_METHOD || "Paypal";
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 2;
