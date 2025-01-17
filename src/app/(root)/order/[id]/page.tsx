26492bcd8cc4f6c4a8b7ee247806ff1314d13dde
VASKO\eqinox <eqinox@abv.bg>
2025-01-17 10:39:58 +0200

    order details page

---
+++ src/app/(root)/order/[id]/page.tsx
@@ -0,0 +1,31 @@
+import { getOrderById } from "@/lib/actions/order.actions";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import OrderDetailsTable from "./order-details-table";
+import { ShippingAddress } from "@/types";
+
+export const metadata: Metadata = {
+  title: "Order Details",
+};
+
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+const OrderDetailsPage: React.FC<Props> = async (props) => {
+  const { id } = await props.params;
+
+  const order = await getOrderById(id);
+  if (!order) notFound();
+
+  return (
+    <OrderDetailsTable
+      order={{
+        ...order,
+        shippingAddress: order.shippingAddress as ShippingAddress,
+      }}
+    />
+  );
+};
+
+export default OrderDetailsPage;
