d06e6213a8696dc0c6f92ac9bbd09f1ea4cf22be
VASKO\eqinox <eqinox@abv.bg>
2025-01-17 16:48:09 +0200

    add tests for paypal

--- src/lib/paypal.ts
+++ src/lib/paypal.ts
@@ -1,16 +1,51 @@
 export const base =
   process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
 
-export const paypal = {};
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: { currency_code: "USD", value: price },
+          },
+        ],
+      }),
+    });
+
+    return handleResponse(response);
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return handleResponse(response);
+  },
+};
 
 // Generate paypal access token
-export async function generateAccessToken() {
+async function generateAccessToken() {
   const { PAYPAL_APP_SECRET, PAYPAL_CLIENT_ID } = process.env;
   const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
     "base64"
   );
 
-  const response = await fetch(`${base}/v1/auth2/token`, {
+  const response = await fetch(`${base}/v1/oauth2/token`, {
     method: "POST",
     body: "grant_type=client_credentials",
     headers: {
@@ -19,11 +54,18 @@ export async function generateAccessToken() {
     },
   });
 
+  const JsonData = await handleResponse(response);
+
+  return JsonData.access_token;
+}
+
+async function handleResponse(response: Response) {
   if (response.ok) {
-    const JsonData = await response.json();
-    return JsonData.access_token;
+    return response.json();
   } else {
     const errorMessage = await response.text();
     throw new Error(errorMessage);
   }
 }
+
+export { generateAccessToken };
