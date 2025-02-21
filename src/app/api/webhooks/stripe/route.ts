ff2d5b247b99835b7b8a62ddf015c187e76d12f2
VASKO\eqinox <eqinox@abv.bg>
2025-02-21 11:34:10 +0200

    change webhook secret

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { updateOrderToPaid } from "@/lib/actions/order.actions";
 
 // Initialize Stripe with the secret API key from environment variables
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET as string);
 
 // LOGGING FUNCTION
 // eslint-disable-next-line
